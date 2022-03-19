8c4fffb83d44cbff3dd5f1664557094e4b7acc8f
masfranzhuo <masfranzhuo@gmail.com>
2022-03-19 23:32:02 +0700

    update smart contract data type

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,13 +2,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Counter {
-  int256 public counter;
+  int public counter;
 
   constructor() {
     counter = 0;
   }
 
-  function incrementCounter() public returns(int256) {
+  function incrementCounter() public returns(int) {
     return counter++;
   }
 }
